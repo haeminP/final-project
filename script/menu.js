80ba46107d08d7635eca87a522974ac2a67dc508
Haemin Park <hmin.park1@gmail.com>
2024-03-10 18:37:35 -0400

    first commit

---
+++ script/menu.js
@@ -0,0 +1,7 @@
+(function (d) {
+  const $nav = d.querySelector('nav');
+  const $btn = d.querySelector('.btn-menu');
+  $btn.addEventListener('click', function () {
+    $nav.classList.toggle('show');
+  });
+})(document);
